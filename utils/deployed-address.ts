e9f0b72fea4cfb4775a1ad3ab2dd58262422dc61
peliumin <118102141+peliumin@users.noreply.github.com>
2024-08-10 08:51:00 +0000

    first commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x281375E541d08db0d0E1E4e58f2BE9A4088A9818'
+export const ProxyAdmin = '0xFd8Da496C3dfAA7F8ff837386a79a323ba44bFF9'
+export const SWTRImplementation = '0xc1d3302F6da64c0b8Deb56CB9865Eb44783142aA'
     
